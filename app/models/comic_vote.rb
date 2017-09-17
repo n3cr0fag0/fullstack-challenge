4e34982ba1443baa534301ef624d171ab420c275
Necro Fago <n3cr0fag0@gmail.com>
2017-09-17 16:39:20 +0100

    Fix upvotes not being saved

--- app/models/comic_vote.rb
+++ app/models/comic_vote.rb
@@ -2,6 +2,7 @@ class ComicVote < ApplicationRecord
   def self.upvote(comic_id:)
     find_or_initialize_by(comic_id: comic_id).tap do |votes|
       votes.votes += 1
+      votes.save
     end
   end
 end
